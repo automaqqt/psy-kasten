5134ccd6bbded9f3845194bd63cc9e3842388b48
fkuem <59531560+automaqqt@users.noreply.github.com>
2025-03-31 01:33:42 +0200

    add analytics

--- pages/_document.js
+++ pages/_document.js
@@ -8,9 +8,10 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#0070f3" />
-          <meta name="description" content="Digital implementation of the Corsi block-tapping test for assessing visuo-spatial short term working memory" />
+          <meta name="description" content="PSYKASTEN" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <script defer src="https://analysis.cryptomonkeys.cc/script.js" data-website-id="2e2456f5-72a7-4815-b750-0a70f96a4cb2"></script>
         </Head>
         <body>
           <Main />
